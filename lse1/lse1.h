e626e2ff1f48e6cce71840555db4392fa39dadbc
andreyPogodin <andreyweather@gmail.com>
2024-04-18 14:09:35 +0300

    commit

---
+++ lse1/lse1.h
@@ -0,0 +1,18 @@
+//
+//  lse1.h
+//  lse1
+//
+//  Created by andrey.pogodin on 18.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lse1.
+FOUNDATION_EXPORT double lse1VersionNumber;
+
+//! Project version string for lse1.
+FOUNDATION_EXPORT const unsigned char lse1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lse1/PublicHeader.h>
+
+
